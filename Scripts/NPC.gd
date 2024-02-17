b56aa5e4ece9c29dacb176cbd4c645714f6ab19e
StaticYolt <leochengsg@gmail.com>
2024-02-17 09:26:06 -0500

    first commit

---
+++ Scripts/NPC.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@export var hunger = 0
+#func _ready():
+#	print("Hello World")
+func _process(delta):
+#	print(find_child("UtilityAiAgent")._action_scores)
+	_handle_hunger(delta)
+#	print(hunger)
+
+func _handle_hunger(delta):
+	hunger = clampf(hunger + delta * 10, 0, 100)
+
+func _eat():
+	hunger = 0
+
+func _on_utility_ai_agent_top_score_action_changed(top_action_id):
+	print("Action changed: %s" % top_action_id)
+	
+	match top_action_id:
+		"eat":
+			_eat()
